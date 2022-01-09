cb5a140caa4996749e343d8ef27894315ac29123
Babin Bohora <bohorababin1@gmail.com>
2022-01-09 16:01:46 +0900

    model generated and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting all articles"
+Article.destroy_all
+
+puts "Seeding articles"
+
+100.times do
+  Article.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph
+  )
+end
+
+puts "done"
